03e82d70ce7696ca7571f8e2a82a310f7da43614
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2024-01-08 00:58:54 +0100

    test

--- main.tf
+++ main.tf
@@ -22,11 +22,6 @@ module "network" {
       subnet_region = var.region
     },
     {
-      subnet_name   = "test"
-      subnet_ip     = "10.101.255.0/24"
-      subnet_region = var.region
-    },
-    {
       subnet_name   = local.aaa_gke_cluster_lb_spec_name
       subnet_ip     = local.aaa_gke_cluster_lb_ip_range
       subnet_region = var.region
